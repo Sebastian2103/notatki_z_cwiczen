9ae59f97bc83dc3a4b66b49cdcc606eb789a9258
Sebastian Pająk <42960067+Sebastian2103@users.noreply.github.com>
2022-04-13 14:07:14 +0200

    Create lab_6.py

---
+++ lab_6.py
@@ -0,0 +1,47 @@
+import numpy as np
+import math
+
+
+# a = np.array([20,30,40,50])
+# b = np.arange(4)
+# c = a+b
+# print (c)
+#
+# print (b**2)
+#
+# print(np.sqrt(b))
+# print (np.exp(b))
+#
+# d = np.exp(b) + a
+# print(d)
+
+# a = np.arange(12).reshape((3, 4))
+# print(a)
+#  print(a.sum())
+#  print(a.sum(axis=0))
+#  print(a.sum(axis=1))
+#
+# print(a.cumsum(axis=1))
+
+# a = np.arange(3)
+# b = np.arange(3)
+#
+# c = a.dot(b)
+# print(c)
+
+# d = np.array([[3,4,1],[4,6,2]])
+# e = np.array([2,4],[1,3],[2,5])
+# f = d.dot(e)
+# print(f)
+
+# print(a)
+# # for b in a.flat:
+# #     print(b)
+
+a = np.arange(12)
+print(a)
+b = a.reshape((3,4))
+c = a.reshape((4,3))
+print(b)
+print(c)
+d = c.rave
